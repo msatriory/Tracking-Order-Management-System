3cd6a5114c23634b89d9e66c7073fd35830c1830
sayamuhammad <sayamuhammad_satrio@yahoo.co.id>
2015-08-07 13:25:49 -0700

    update 8-7-2015

--- toms/editableTablewithjQuery/source/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	define('DB','your_database_name');
-	define('USER','your_user_name');
-	define('PWD','your_password');
-
-	$dbh = new PDO('mysql:host=localhost;dbname='.DB,USER,PWD);
-
-?>
